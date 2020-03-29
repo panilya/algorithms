a1244b720319caff9ac1fbe4c98bd77e443a8db6
Ilya Pantsir <wethands04@gmail.com>
2020-03-29 23:37:48 +0300

    CountingValleys Hackerrank

---
+++ HackerRank/CountingValleys.java
@@ -0,0 +1,19 @@
+public class Solution {
+    static int countingValleys(int n, String s) {
+
+        int altitude = 0;
+        int num_valleys = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'U') {
+                if (altitude == -1) {
+                    num_valleys++;
+                }
+                altitude++;
+            }
+            if (s.charAt(i) == 'D') {
+                altitude--;
+            }
+        }
+        return num_valleys;
+    }
